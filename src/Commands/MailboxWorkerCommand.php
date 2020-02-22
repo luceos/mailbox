e8ebcd97b9640a985ef46af1d72ebc8b96a08f46
Daniël Klabbers <daniel@klabbers.email>
2020-02-22 06:53:42 +0100

    confirm to fof command prefix

--- src/Commands/MailboxWorkerCommand.php
+++ src/Commands/MailboxWorkerCommand.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 
 class MailboxWorkerCommand extends Command
 {
-    protected $signature = 'mailbox:work';
+    protected $signature = 'fof:mailbox:work';
     protected $description = 'Executes mailbox processing.';
 
     public function handle(MailboxRepository $mailboxes, Importer $importer)
